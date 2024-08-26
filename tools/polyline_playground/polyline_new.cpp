61fa4513b393b8e34cb2e7daa08b3e650ad68102
Michał Cichoń <michcic@gmail.com>
2024-08-26 19:32:19 +0200

    [Polyline] First implementation (fixup)

--- tools/polyline_playground/polyline_new.cpp
+++ tools/polyline_playground/polyline_new.cpp
@@ -8,13 +8,26 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
     if (count < 2 || thickness <= 0.0f) [[unlikely]]
         return;
 
+    // Internal join types:
+    //   - Miter: default join type, sharp point
+    //   - MiterClip: clipped miter join, basically a bevel join with a limited length
+    //   - Bevel: straight bevel join between segments
+    //   - Butt: flat cap
+    //   - ThickButt: flat cap with extra vertices, used for discontinuity between segments
+    enum JoinType { Miter, MiterClip, Bevel, Butt, ThickButt };
+
     const bool        closed         = !!(draw_flags & ImDrawFlags_Closed) && (count > 2);
     const bool        antialias      = !!(draw_list->Flags & ImDrawListFlags_AntiAliasedLines);
-    const ImDrawFlags join           = draw_flags & ImDrawFlags_JoinMask_;
-    const ImDrawFlags cap            = draw_flags & ImDrawFlags_CapMask_;
-    const float       half_thickness = thickness * 0.5f;
+    const ImDrawFlags join_flags     = draw_flags & ImDrawFlags_JoinMask_;
+    const ImDrawFlags cap_flags      = draw_flags & ImDrawFlags_CapMask_;
+    const ImDrawFlags join           = join_flags ? join_flags : ImDrawFlags_JoinDefault_;
+    const ImDrawFlags cap            = cap_flags ? cap_flags : ImDrawFlags_CapDefault_;
+    
+    // Pick default join type based on join flags
+    const int default_join_type       = join == ImDrawFlags_JoinBevel ? Bevel : (join == ImDrawFlags_JoinMiterClip ? MiterClip : Miter);
+    const int default_join_limit_type = join == ImDrawFlags_JoinMiterClip ? MiterClip : Bevel;
 
-    // Compute normals
+    // Compute segment normals and lengths
     ImVec2* normals = nullptr;
     float* segments_length_sqr = nullptr;
     {
@@ -76,15 +89,19 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
         draw_list->_IdxWritePtr[N * 3 + 1] = idx_base + B;      \
         draw_list->_IdxWritePtr[N * 3 + 2] = idx_base + C
 
-    const float clamped_miter_limit = ImMax(1.0f, miter_limit);
+    // Most dimensions are squares of the actual values, fit nicely with trigonometric identities
+    const float half_thickness     = thickness * 0.5f;
+    const float half_thickness_sqr = half_thickness * half_thickness;
+
+    const float clamped_miter_limit = ImMax(0.0f, miter_limit);
     const float miter_distance_limit = half_thickness * clamped_miter_limit;
     const float miter_distance_limit_sqr = miter_distance_limit * miter_distance_limit;
 
     // miter square formula is derived here: https://www.angusj.com/clipper2/Docs/Trigonometry.htm
     const float miter_angle_limit = 2.0f / (clamped_miter_limit * clamped_miter_limit) - 1.0f;
 
-    const float half_thickness_sqr = half_thickness * half_thickness;
-
+    // Reserve vertices and indices for worst case scenario
+    // Unused vertices and indices will be released after the loop
     const auto uv             = draw_list->_Data->TexUvWhitePixel;
     const auto vtx_count      = count * 6 + 2; // top 6 vertices per join, 2 vertices per butt cap
     const auto idx_count      = count * 4 * 3; // top 4 triangles per join
@@ -95,7 +112,8 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
     const auto vtx_start = draw_list->_VtxWritePtr;
     const auto idx_start = draw_list->_IdxWritePtr;
 
-    // Start with butt cap
+    // Last two vertices in the vertex buffer are reserved to next segment to build upon
+    // This is true for all segments.
     ImVec2 p0 =    data[closed ? count - 1 : 0];
     ImVec2 n0 = normals[closed ? count - 1 : 0];
 
@@ -103,10 +121,6 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
     IM_POLYLINE_VERTEX(1, p0.x + n0.x * half_thickness, p0.y + n0.y * half_thickness);
 
     draw_list->_VtxWritePtr += 2;
-
-    enum JoinType { Miter, Bevel, Butt, ThickButt };
-
-    const int default_join_type = join == ImDrawFlags_JoinBevel ? Bevel : Miter;
 
     int last_join_type = Butt;
 
@@ -120,37 +134,63 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
         const float sin_theta = n0.y * n1.x - n0.x * n1.y;
 
         // miter offset formula is derived here: https://www.angusj.com/clipper2/Docs/Trigonometry.htm
-        const float miter_scale_factor = cos_theta > -1.0f ? half_thickness / (1.0f + cos_theta) : FLT_MAX;
+        const float miter_scale_factor = cos_theta > -0.995f ? half_thickness / (1.0f + cos_theta) : FLT_MAX;
         const float miter_offset_x = (n0.x + n1.x) * miter_scale_factor;
         const float miter_offset_y = (n0.y + n1.y) * miter_scale_factor;
 
+        // always have to know if join miter is limited
         const float miter_distance_sqr = miter_offset_x * miter_offset_x + miter_offset_y * miter_offset_y;
         const bool  limit_miter        = (miter_distance_sqr > miter_distance_limit_sqr) || (cos_theta < miter_angle_limit);
 
+        // check for discontinuity, miter distance greater than segment lengths will mangle geometry
+        // so we create disconnect and create overlapping geometry just to keep overall shape correct
         const float segment_length_sqr      = segments_length_sqr[i];
         const float next_segment_length_sqr = segments_length_sqr[i + 1];
-
         const bool  disconnected       = (segment_length_sqr < miter_distance_sqr) || (next_segment_length_sqr < miter_distance_sqr) || (segment_length_sqr < half_thickness_sqr);
-        const int   ideal_join_type    = !closed && (i == count - 1) ? Butt : (limit_miter ? Bevel : default_join_type);
-        const int   join_type          = disconnected ? ThickButt : ideal_join_type;
+
+        // select join type
+        int preferred_join_type = Butt;
+        if (closed || !(i == count - 1)) [[likely]]
+        {
+            preferred_join_type = limit_miter ? default_join_limit_type : default_join_type;
+
+            // MiterClip need to be 'clamped' to Bevel if too short or to Miter if clip is not necessary
+            if (preferred_join_type == MiterClip)
+            {
+                const float miter_clip_min_distance_sqr = 0.5f * half_thickness_sqr * (cos_theta + 1);
+
+                if (miter_distance_limit_sqr < miter_clip_min_distance_sqr)
+                    preferred_join_type = Bevel;
+                else if (miter_distance_sqr < miter_distance_limit_sqr)
+                    preferred_join_type = Miter;
+            }
+        }
+
+        // Actual join used does account for discontinuity
+        const int join_type = disconnected ? ThickButt : preferred_join_type;
 
 #if 0
+        float xxx = sin_theta / (1.0f + cos_theta);
+
         ImDrawList* draw_list_2 = ImGui::GetForegroundDrawList();
         ImGuiTextBuffer debug_text;
-        debug_text.appendf("i: %d, a: %.2f, c: %.2f, m: %.2f (max: %.2f), s: %.2f, limit: %d, disc: %d\n",
+        debug_text.appendf("i: %d, a: %.2f, c: %.2f, m: %.2f (max: %.2f), s: %.2f, limit: %d, disc: %d, xxx: %.2f\n",
             i,
             ImAcos(ImClamp(cos_theta, -1.0f, 1.0f)) * 180 / IM_PI,
             sin_theta,
             ImSqrt(miter_distance_sqr), ImSqrt(miter_distance_limit_sqr),
             ImSqrt(segment_length_sqr),
             limit_miter ? 1 : 0,
-            disconnected ? 1 : 0
+            disconnected ? 1 : 0,
+            xxx
         );
         draw_list_2->AddText(ImVec2(10, 10 + i * ImGui::GetTextLineHeight()), IM_COL32(255, 255, 255, 255), debug_text.begin());
 #endif
 
         int new_vtx_count = 0;
 
+        // Joins try to emit as little vertices as possible.
+        // Last two vertices will be reused by next segment.
         if (join_type == Miter) [[likely]]
         {
             IM_POLYLINE_VERTEX(0, p1.x - miter_offset_x, p1.y - miter_offset_y);
@@ -173,6 +213,62 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
             }
             new_vtx_count = 3;
         }
+        else if (join_type == MiterClip) [[unlikely]]
+        {
+            // Note: MiterClip does require sqrt() because we have to compute point on the clip line
+            //       which is not the same as miter offset point
+
+            ImVec2 clip_line_direction = ImVec2(n0.x + n1.x, n0.y + n1.y);
+            const float clip_line_normal_sqr = clip_line_direction.x * clip_line_direction.x + clip_line_direction.y * clip_line_direction.y;
+            if (clip_line_normal_sqr > 0.0f) [[likely]]
+            {
+                const float clip_line_inv_len = ImRsqrt(clip_line_normal_sqr);
+                clip_line_direction.x *= clip_line_inv_len;
+                clip_line_direction.y *= clip_line_inv_len;
+            }
+
+            const auto clip_projection = n0.y * clip_line_direction.x - n0.x * clip_line_direction.y;
+
+            if (sin_theta < 0.0f)
+            {
+                IM_POLYLINE_VERTEX(0, p1.x + n0.x * half_thickness, p1.y + n0.y * half_thickness);
+                IM_POLYLINE_VERTEX(1, p1.x - miter_offset_x,        p1.y - miter_offset_y);
+                IM_POLYLINE_VERTEX(2, p1.x + n1.x * half_thickness, p1.y + n1.y * half_thickness);
+
+                // We do not care for degenerate case when two segments are parallel to each
+                // other because this case will always be handled by discontinuity in ThickButt
+                if (clip_projection != 0) [[likely]]
+                {
+                    const auto clip_line_point   = ImVec2(p1.x + clip_line_direction.x * miter_distance_limit, p1.y + clip_line_direction.y * miter_distance_limit);
+                    const auto clip_point_offset = (n0.x * (clip_line_point.x - draw_list->_VtxWritePtr[0].pos.x) + n0.y * (clip_line_point.y - draw_list->_VtxWritePtr[0].pos.y)) / clip_projection;
+
+                    draw_list->_VtxWritePtr[0].pos.x = clip_line_point.x + (clip_point_offset * clip_line_direction.y);
+                    draw_list->_VtxWritePtr[0].pos.y = clip_line_point.y - (clip_point_offset * clip_line_direction.x);
+                    draw_list->_VtxWritePtr[2].pos.x = clip_line_point.x - (clip_point_offset * clip_line_direction.y);
+                    draw_list->_VtxWritePtr[2].pos.y = clip_line_point.y + (clip_point_offset * clip_line_direction.x);
+                }
+            }
+            else
+            {
+                IM_POLYLINE_VERTEX(0, p1.x - n0.x * half_thickness, p1.y - n0.y * half_thickness);
+                IM_POLYLINE_VERTEX(1, p1.x - n1.x * half_thickness, p1.y - n1.y * half_thickness);
+                IM_POLYLINE_VERTEX(2, p1.x + miter_offset_x,        p1.y + miter_offset_y);
+
+                // We do not care for degenerate case when two segments are parallel to each
+                // other because this case will always be handled by discontinuity in ThickButt
+                if (clip_projection != 0) [[likely]]
+                {
+                    const auto clip_line_point = ImVec2(p1.x - clip_line_direction.x * miter_distance_limit, p1.y - clip_line_direction.y * miter_distance_limit);
+                    const auto clip_point_offset = (n0.x * (clip_line_point.x - draw_list->_VtxWritePtr[0].pos.x) + n0.y * (clip_line_point.y - draw_list->_VtxWritePtr[0].pos.y)) / clip_projection;
+
+                    draw_list->_VtxWritePtr[0].pos.x = clip_line_point.x + (clip_point_offset * clip_line_direction.y);
+                    draw_list->_VtxWritePtr[0].pos.y = clip_line_point.y - (clip_point_offset * clip_line_direction.x);
+                    draw_list->_VtxWritePtr[1].pos.x = clip_line_point.x - (clip_point_offset * clip_line_direction.y);
+                    draw_list->_VtxWritePtr[1].pos.y = clip_line_point.y + (clip_point_offset * clip_line_direction.x);
+                }
+            }
+            new_vtx_count = 3;
+        }
         else if (join_type == Butt) [[unlikely]]
         {
             IM_POLYLINE_VERTEX(0, p1.x - n0.x * half_thickness, p1.y - n0.y * half_thickness);
@@ -183,13 +279,16 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
         }
         else if (join_type == ThickButt) [[unlikely]]
         {
+            // 2 and 3 vertices are reserved for individual join types
             IM_POLYLINE_VERTEX(0, p1.x - n0.x * half_thickness, p1.y - n0.y * half_thickness);
             IM_POLYLINE_VERTEX(1, p1.x + n0.x * half_thickness, p1.y + n0.y * half_thickness);
             IM_POLYLINE_VERTEX(4, p1.x - n1.x * half_thickness, p1.y - n1.y * half_thickness);
             IM_POLYLINE_VERTEX(5, p1.x + n1.x * half_thickness, p1.y + n1.y * half_thickness);
             new_vtx_count = 6;
 
-            if (ideal_join_type == Bevel) [[unlikely]]
+            // ThickButt is always a discontinuity, yet we care here to fill the joins
+            // and reuse of ThickButt vertices
+            if (preferred_join_type == Bevel) [[unlikely]]
             {
                 IM_POLYLINE_VERTEX(2, p1.x, p1.y);
 
@@ -204,7 +303,7 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
 
                 draw_list->_IdxWritePtr += 3;
             }
-            else if (ideal_join_type == Miter) [[unlikely]]
+            else if (preferred_join_type == Miter) [[unlikely]]
             {
                 IM_POLYLINE_VERTEX(2, p1.x, p1.y);
 
@@ -223,9 +322,24 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
                     draw_list->_IdxWritePtr += 6;
                 }
             }
+            else if (preferred_join_type == MiterClip) [[unlikely]]
+            {
+                IM_POLYLINE_VERTEX(2, p1.x, p1.y);
+
+                if (sin_theta < 0.0f)
+                {
+                    IM_POLYLINE_TRIANGLE(0, 3, 7, 4);
+                }
+                else
+                {
+                    IM_POLYLINE_TRIANGLE(0, 2, 4, 6);
+                }
+
+                draw_list->_IdxWritePtr += 3;
+            }
         }
 
-        if (join_type == Bevel)
+        if (join_type == Bevel || join_type == MiterClip)
         {
             if (sin_theta < 0.0f)
             {
--- tools/polyline_playground/polyline_new.h
+++ tools/polyline_playground/polyline_new.h
@@ -7,14 +7,18 @@ namespace ImGuiEx {
 
 enum ImDrawFlagsExtra
 {
-    ImDrawFlags_JoinBevel = 1 << 9,
-    ImDrawFlags_JoinRound = 2 << 9,
-    ImDrawFlags_JoinMiter = 3 << 9,
-    ImDrawFlags_JoinMask_ = 3 << 9,
+    ImDrawFlags_JoinBevel       = 1 << 9,
+    ImDrawFlags_JoinRound       = 2 << 9,
+    ImDrawFlags_JoinMiter       = 3 << 9,
+    ImDrawFlags_JoinMiterClip   = 4 << 9,
+    ImDrawFlags_JoinDefault_    = ImDrawFlags_JoinMiter,
+    ImDrawFlags_JoinMask_       = 7 << 9,
 
-    ImDrawFlags_CapSquare = 1 << 11,
-    ImDrawFlags_CapRound  = 2 << 11,
-    ImDrawFlags_CapMask_  = 3 << 11,
+    ImDrawFlags_CapButt         = 1 << 12,
+    ImDrawFlags_CapSquare       = 2 << 12,
+    ImDrawFlags_CapRound        = 3 << 12,
+    ImDrawFlags_CapDefault_     = ImDrawFlags_CapButt,
+    ImDrawFlags_CapMask_        = 3 << 12,
 };
 
 void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, int count, ImU32 color, ImDrawFlags draw_flags, float thickness, float miter_limit = 20.0f);
