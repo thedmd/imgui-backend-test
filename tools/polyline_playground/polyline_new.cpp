faa0f0992ef6b739b23ecca14d0a94f8fc910f5d
Michał Cichoń <michcic@gmail.com>
2024-08-26 05:35:09 +0200

    [Polyline] First implementation (fixup)

--- tools/polyline_playground/polyline_new.cpp
+++ tools/polyline_playground/polyline_new.cpp
@@ -3,13 +3,9 @@
 
 namespace ImGuiEx {
 
-#define IM_NORMALIZE2F_OVER_ZERO(VX,VY)     { float d2 = VX*VX + VY*VY; if (d2 > 0.0f) { float inv_len = ImRsqrt(d2); VX *= inv_len; VY *= inv_len; } } (void)0
-#define IM_FIXNORMAL2F_MAX_INVLEN2          100.0f // 500.0f (see #4053, #3366)
-#define IM_FIXNORMAL2F(VX,VY)               { float d2 = VX*VX + VY*VY; if (d2 > 0.000001f) { float inv_len2 = 1.0f / d2; if (inv_len2 > IM_FIXNORMAL2F_MAX_INVLEN2) inv_len2 = IM_FIXNORMAL2F_MAX_INVLEN2; VX *= inv_len2; VY *= inv_len2; } } (void)0
-
 void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int count, const ImU32 color, const ImDrawFlags draw_flags, float thickness, float miter_limit)
 {
-    if (count < 2)
+    if (count < 2 || thickness <= 0.0f) [[unlikely]]
         return;
 
     const bool        closed         = !!(draw_flags & ImDrawFlags_Closed) && (count > 2);
@@ -84,13 +80,15 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
     const float miter_distance_limit = half_thickness * clamped_miter_limit;
     const float miter_distance_limit_sqr = miter_distance_limit * miter_distance_limit;
 
+    // miter square formula is derived here: https://www.angusj.com/clipper2/Docs/Trigonometry.htm
+    const float miter_angle_limit = 2.0f / (clamped_miter_limit * clamped_miter_limit) - 1.0f;
+
     const float half_thickness_sqr = half_thickness * half_thickness;
 
     const auto uv             = draw_list->_Data->TexUvWhitePixel;
     const auto vtx_count      = count * 6 + 2; // top 6 vertices per join, 2 vertices per butt cap
     const auto idx_count      = count * 4 * 3; // top 4 triangles per join
-    const auto idx_base_start = draw_list->_VtxCurrentIdx;
-    auto       idx_base       = idx_base_start;
+    auto       idx_base       = draw_list->_VtxCurrentIdx;
 
     draw_list->PrimReserve(idx_count, vtx_count);
 
@@ -107,6 +105,8 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
     draw_list->_VtxWritePtr += 2;
 
     enum JoinType { Miter, Bevel, Butt, ThickButt };
+
+    const int default_join_type = join == ImDrawFlags_JoinBevel ? Bevel : Miter;
 
     int last_join_type = Butt;
 
@@ -125,33 +125,37 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
         const float miter_offset_y = (n0.y + n1.y) * miter_scale_factor;
 
         const float miter_distance_sqr = miter_offset_x * miter_offset_x + miter_offset_y * miter_offset_y;
-        const bool  limit_miter        = miter_distance_sqr > miter_distance_limit_sqr;
+        const bool  limit_miter        = (miter_distance_sqr > miter_distance_limit_sqr) || (cos_theta < miter_angle_limit);
 
         const float segment_length_sqr      = segments_length_sqr[i];
         const float next_segment_length_sqr = segments_length_sqr[i + 1];
 
         const bool  disconnected       = (segment_length_sqr < miter_distance_sqr) || (next_segment_length_sqr < miter_distance_sqr) || (segment_length_sqr < half_thickness_sqr);
-        const int   ideal_join_type    = !closed && (i == count - 1) ? Butt : (limit_miter || join == ImDrawFlags_JoinBevel ? Bevel : Miter);
+        const int   ideal_join_type    = !closed && (i == count - 1) ? Butt : (limit_miter ? Bevel : default_join_type);
         const int   join_type          = disconnected ? ThickButt : ideal_join_type;
 
-        //ImDrawList* draw_list_2 = ImGui::GetForegroundDrawList();
-        //ImGuiTextBuffer debug_text;
-        //debug_text.appendf("i: %d, a: %.2f, c: %.2f, m: %.2f (max: %.2f), s: %.2f, limit: %d, disc: %d\n",
-        //    i,
-        //    ImAcos(ImClamp(cos_theta, -1.0f, 1.0f)) * 180 / IM_PI,
-        //    sin_theta,
-        //    ImSqrt(miter_distance_sqr), ImSqrt(miter_distance_limit_sqr),
-        //    ImSqrt(segment_length_sqr),
-        //    limit_miter ? 1 : 0,
-        //    disconnected ? 1 : 0
-        //);
-        //draw_list_2->AddText(ImVec2(10, 10 + i * ImGui::GetTextLineHeight()), IM_COL32(255, 255, 255, 255), debug_text.begin());
+#if 0
+        ImDrawList* draw_list_2 = ImGui::GetForegroundDrawList();
+        ImGuiTextBuffer debug_text;
+        debug_text.appendf("i: %d, a: %.2f, c: %.2f, m: %.2f (max: %.2f), s: %.2f, limit: %d, disc: %d\n",
+            i,
+            ImAcos(ImClamp(cos_theta, -1.0f, 1.0f)) * 180 / IM_PI,
+            sin_theta,
+            ImSqrt(miter_distance_sqr), ImSqrt(miter_distance_limit_sqr),
+            ImSqrt(segment_length_sqr),
+            limit_miter ? 1 : 0,
+            disconnected ? 1 : 0
+        );
+        draw_list_2->AddText(ImVec2(10, 10 + i * ImGui::GetTextLineHeight()), IM_COL32(255, 255, 255, 255), debug_text.begin());
+#endif
+
+        int new_vtx_count = 0;
 
         if (join_type == Miter) [[likely]]
         {
             IM_POLYLINE_VERTEX(0, p1.x - miter_offset_x, p1.y - miter_offset_y);
             IM_POLYLINE_VERTEX(1, p1.x + miter_offset_x, p1.y + miter_offset_y);
-            draw_list->_VtxWritePtr += 2;
+            new_vtx_count = 2;
         }
         else if (join_type == Bevel) [[likely]]
         {
@@ -167,8 +171,7 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
                 IM_POLYLINE_VERTEX(1, p1.x - n1.x * half_thickness, p1.y - n1.y * half_thickness);
                 IM_POLYLINE_VERTEX(2, p1.x + miter_offset_x,        p1.y + miter_offset_y);
             }
-
-            draw_list->_VtxWritePtr += 3;
+            new_vtx_count = 3;
         }
         else if (join_type == Butt) [[unlikely]]
         {
@@ -176,7 +179,7 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
             IM_POLYLINE_VERTEX(1, p1.x + n0.x * half_thickness, p1.y + n0.y * half_thickness);
             IM_POLYLINE_VERTEX(2, p1.x - n1.x * half_thickness, p1.y - n1.y * half_thickness);
             IM_POLYLINE_VERTEX(3, p1.x + n1.x * half_thickness, p1.y + n1.y * half_thickness);
-            draw_list->_VtxWritePtr += 4;
+            new_vtx_count = 4;
         }
         else if (join_type == ThickButt) [[unlikely]]
         {
@@ -184,6 +187,7 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
             IM_POLYLINE_VERTEX(1, p1.x + n0.x * half_thickness, p1.y + n0.y * half_thickness);
             IM_POLYLINE_VERTEX(4, p1.x - n1.x * half_thickness, p1.y - n1.y * half_thickness);
             IM_POLYLINE_VERTEX(5, p1.x + n1.x * half_thickness, p1.y + n1.y * half_thickness);
+            new_vtx_count = 6;
 
             if (ideal_join_type == Bevel) [[unlikely]]
             {
@@ -219,7 +223,6 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
                     draw_list->_IdxWritePtr += 6;
                 }
             }
-            draw_list->_VtxWritePtr += 6;
         }
 
         if (join_type == Bevel)
@@ -245,13 +248,8 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
             draw_list->_IdxWritePtr += 6;
         }
 
-        switch (join_type)
-        {
-            case Butt:      idx_base += 4; break;
-            case ThickButt: idx_base += 6; break;
-            case Bevel:     idx_base += 3; break;
-            case Miter:     idx_base += 2; break;
-        }
+        draw_list->_VtxWritePtr += new_vtx_count;
+        idx_base += new_vtx_count;
 
         p0 = p1;
         n0 = n1;
@@ -259,12 +257,31 @@ void ImDrawList_Polyline(ImDrawList* draw_list, const ImVec2* data, const int co
         last_join_type = join_type;
     }
 
-    idx_base += 2;
-
     if (closed)
     {
+        idx_base += 2;
+
         vtx_start[0].pos = draw_list->_VtxWritePtr[-2].pos;
         vtx_start[1].pos = draw_list->_VtxWritePtr[-1].pos;
+    }
+    else
+    {
+        draw_list->_VtxWritePtr -= 2;
+
+        if (cap == ImDrawFlags_CapSquare) [[unlikely]]
+        {
+            const ImVec2 n0 = normals[0];
+            const ImVec2 n1 = normals[count - 1];
+
+            vtx_start[0].pos.x                += n0.y * half_thickness;
+            vtx_start[0].pos.y                -= n0.x * half_thickness;
+            vtx_start[1].pos.x                += n0.y * half_thickness;
+            vtx_start[1].pos.y                -= n0.x * half_thickness;
+            draw_list->_VtxWritePtr[-2].pos.x -= n1.y * half_thickness;
+            draw_list->_VtxWritePtr[-2].pos.y += n1.x * half_thickness;
+            draw_list->_VtxWritePtr[-1].pos.x -= n1.y * half_thickness;
+            draw_list->_VtxWritePtr[-1].pos.y += n1.x * half_thickness;
+        }
     }
 
     const int used_vtx_count = static_cast<int>(draw_list->_VtxWritePtr - vtx_start);
